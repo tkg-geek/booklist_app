1f37f62f76557735eb2d618970400cf293a337ce
tkg-geek <tkg.geek@gmail.com>
2024-12-03 11:57:09 +0900

    dev innit

--- inc/functions.php
+++ inc/functions.php
@@ -1,15 +1,23 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 function str2html(string $string) :string {
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 }
 function db_open() :PDO {
-    $user = "phpuser";
-    $password = "1ye.y8O*ty7cwgRx";
+    $host = $_ENV['DB_HOST'];
+    $dbname = $_ENV['DB_NAME'];
+    $user = $_ENV['DB_USER'];
+    $password = $_ENV['DB_PASSWORD'];
     $opt = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_EMULATE_PREPARES => false,
         PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
     ];
-    $dbh = new PDO('mysql:host=localhost;dbname=sample_db', $user, $password, $opt);
+    $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $password, $opt);
     return $dbh;
 }
